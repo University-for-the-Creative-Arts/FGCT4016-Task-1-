99e70bbd04e81169f9ab6de13c2dfde1f877a881
Huntertheheavy <153214511+Huntertheheavy@users.noreply.github.com>
2026-02-17 22:15:24 +0000

    Task 1 Done

---
+++ Sem2UniversityTasks/Source/Sem2UniversityTasks/Sem2UniversityTasks.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sem2UniversityTasks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sem2UniversityTasks, "Sem2UniversityTasks" );
